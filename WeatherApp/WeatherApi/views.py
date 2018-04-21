7e58164f23c7c0daf189240e3f557ca39bb6b0cc
Ryan Rodwell <ryanrodwell@gmail.com>
2018-04-21 15:28:11 -0400

    WIP: adding views and ursl for app

--- WeatherApp/WeatherApi/views.py
+++ WeatherApp/WeatherApi/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+
+    # appoint_list = Appointment.objects.filter(
+    #         description__icontains = "D"
+    #     )
+    # appt_dict = {
+    #     'appointment_records': appoint_list,
+    # }
+
+    return render(request,'WeatherApi/index.html')
+
+
+### API function ####
