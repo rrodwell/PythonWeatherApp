1509593d52de44032b8d3ac620069bff13417916
Ryan Rodwell <ryanrodwell@gmail.com>
2018-04-21 15:32:18 -0400

    WIP: updating views

--- WeatherApp/WeatherApi/views.py
+++ WeatherApp/WeatherApi/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from datetime import datetime
+from threading import Timer
+import requests
+import json
+import threading
 
 # Create your views here.
 def index(request):
@@ -14,3 +19,46 @@ def index(request):
 
 
 ### API function ####
+def getWeather(request):
+
+    # hardcoded api address
+
+    api_address = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22Atlanta%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+
+    #JSON
+
+    json_data = requests.get(api_address).json()
+
+    temperature = json_data['query']['results']['channel']['item']['condition']['temp']
+    description = json_data['query']['results']['channel']['item']['condition']['text']
+    date = json_data['query']['results']['channel']['item']['condition']['date']
+    forecast = json_data['query']['results']['channel']['item']['forecast'][0]['high']
+
+    print(description)
+
+    #Emoji dictionary
+
+    emojiList = {
+        'Mostly Cloudy': ':cloud:',
+        'Sunny': ':sunny:' ,
+        'Cloudy': ':cloud:',
+        'Mostly Sunny': ':mostly_sunny:',
+        'Partly Cloudy': ':partly_sunny:',
+        'Breezy': ':wind_blowing_face:',
+        'Heavy Rain': ':rain_cloud:',
+        'Rain': ':rain_cloud:'
+    }
+
+
+    emoji = ''
+
+    for k, v in emojiList.items():
+        if k == description:
+            emoji = v
+
+
+    # print statement
+
+    printme = 'The temperature is ' + temperature + ' degrees F and ' + description + emoji + ' in Atlanta.' + '\n The high today is ' + forecast + '.'
+
+    return printme
