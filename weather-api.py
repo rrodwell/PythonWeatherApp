7c6cc9437dbaf01b2376607f5096ec86660d6321
phillip dacosta <phillip.dacosta@gmail.com>
2018-04-23 10:46:01 -0400

    Updated variable names and added forecast retrievals for tomorrows (next day) weather

--- weather-api.py
+++ weather-api.py
@@ -6,27 +6,29 @@ import threading
 
 
 def getWeather():
+    # hardcoded api address
 
-   # hardcoded api address
+    api_address = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22Atlanta%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
 
-   api_address = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22Atlanta%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+    # JSON
 
-   #JSON
+    json_data = requests.get(api_address).json()
 
-   json_data = requests.get(api_address).json()
+    temperatureNow = json_data['query']['results']['channel']['item']['condition']['temp']
+    descriptionToday = json_data['query']['results']['channel']['item']['condition']['text']
+    date = json_data['query']['results']['channel']['item']['condition']['date']
+    highToday = json_data['query']['results']['channel']['item']['forecast'][0]['high']
+    highTomorrow = json_data['query']['results']['channel']['item']['forecast'][1]['high']
+    descriptionTomorrow = json_data['query']['results']['channel']['item']['forecast'][1]['text']
 
-   temperature = json_data['query']['results']['channel']['item']['condition']['temp']
-   description = json_data['query']['results']['channel']['item']['condition']['text']
-   date = json_data['query']['results']['channel']['item']['condition']['date']
-   forecast = json_data['query']['results']['channel']['item']['forecast'][0]['high']
+    print(forecastTomorrow)
+    print(description)
 
-   print(description)
+    # Emoji dictionary
 
-   #Emoji dictionary
-
-   emojiList = {
+    emojiList = {
         'Mostly Cloudy': ':cloud:',
-        'Sunny': ':sunny:' ,
+        'Sunny': ':sunny:',
         'Cloudy': ':cloud:',
         'Mostly Sunny': ':mostly_sunny:',
         'Partly Cloudy': ':partly_sunny:',
@@ -35,41 +37,40 @@ def getWeather():
         'Rain': ':rain_cloud:'
     }
 
+    emoji = ''
 
-   emoji = ''
+    for k, v in emojiList.items():
+        if k == description:
+            emoji = v
 
-   for k, v in emojiList.items():
-       if k == description:
-           emoji = v
+    # print statement
 
+    printme = 'The temperature is ' + temperatureNow + ' degrees F and ' + descriptionToday + emoji + ' in Atlanta.' + '\n The high today is ' + highToday + '. \n Tomorrow : ' + highTomorrow + " and " + descriptionTomorrow
 
-   # print statement
+    # Slack webhook integration
 
-   printme = 'The temperature is ' + temperature + ' degrees F and ' + description + emoji + ' in Atlanta.' + '\n The high today is ' + forecast + '.'
+    webhook_url = 'https://hooks.slack.com/services/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'
+    # webhook_url = 'https://hooks.slack.com/services/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'
 
-   #Slack webhook integration
+    slack_data = {
 
-   webhook_url = 'https://hooks.slack.com/services/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'
-   #webhook_url = 'https://hooks.slack.com/services/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'
-
-   slack_data = {
-       
         'channel': '#pdtest',
         'username': 'philBot',
         'text': printme,
-        'icon_emoji': ':waffle-fry:'
+
     }
 
-   response = requests.post(
-       webhook_url, data=json.dumps(slack_data),
-       headers={
-           'Content-Type': 'application/json'
-       }
-   )
+    response = requests.post(
+        webhook_url, data=json.dumps(slack_data),
+        headers={
+            'Content-Type': 'application/json'
+        }
+    )
 
-   if response.status_code != 200:
-       raise ValueError(
-           'Request to slack returned an error %s, the response is:\n%s' % (response.status_code, response.text)
-       )
+    if response.status_code != 200:
+        raise ValueError(
+            'Request to slack returned an error %s, the response is:\n%s' % (response.status_code, response.text)
+        )
+
 
 getWeather()
